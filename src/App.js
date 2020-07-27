690875055dc8855078e8e25c5db967ea400a5d33
gmurdoko <galangcahyomurdoko@gmail.com>
2020-07-27 13:35:55 +0700

    curd redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-// import Nav2 from "./components/router/Nav2";
-import Nav from "./components/router/Nav";
-// import Hoc from "./container/Hoc";
-// import HocComponent from "./challengeHoc/hocComponent";
 
-// import SamplePropTypes from "./components/SamplePropTypes";
+import Nav from "./components/router/Nav";
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                {/* <HocComponent /> */}
-                {/* <Hoc /> */}
-                {/* <Nav2 /> */}
                 <Nav />
-                {/* <SamplePropTypes>
-                    <div />
-                </SamplePropTypes> */}
             </BrowserRouter>
         );
     }
